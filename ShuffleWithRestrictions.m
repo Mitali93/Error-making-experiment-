9574754952ddf91758a8e90670bf03d9ad269b66
Mitali <33693459+Mitali93@users.noreply.github.com>
2017-12-15 21:21:26 +0000

    Add files via upload

--- ShuffleWithRestrictions.m
+++ ShuffleWithRestrictions.m
@@ -2,30 +2,45 @@ function [AllResults, order, shuffled] = ShuffleWithRestrictions(trials)
 try
     tnum = trials(:,1);
     tcor = (trials(:,7) == 0);
+    twrong=(trials(:,7)~=0);
     
     
     ntrials = length(tnum);
     maxcorrect = 4;
+    maxwrong=4;
     order = nan(ntrials,1);
     
     consecutiveCorrect = 0;
+    consecutiveWrong=0;
     
     for i = 1 : ntrials
         
         % draw a sample from tnum
-        if consecutiveCorrect == maxcorrect
+        if consecutiveCorrect == maxcorrect  
             tries = 0;
-            while iscorrect == true
+            while iscorrect == true  
                 [Y,index] = Sample(tnum);
-                iscorrect = tcor(index);
+                iscorrect = tcor(index);   
                 tries = tries + 1;
                 if tries > 50 % time out at some point and try again
                     error('Trying again')
                 end
             end
-        elseif consecutiveCorrect < maxcorrect
+        elseif consecutiveWrong == maxwrong
+            tries = 0;
+            while iswrong == true
+                [Y,index] = Sample(tnum);
+                iswrong=twrong(index);
+                tries = tries + 1;
+                if tries > 50 % time out at some point and try again
+                    error('Trying again')
+                end
+            end
+                
+        elseif consecutiveCorrect < maxcorrect && consecutiveWrong<maxwrong
             [Y,index] = Sample(tnum);
             iscorrect = tcor(index);
+            iswrong= twrong(index);
             
         end
         
@@ -35,15 +50,22 @@ try
             consecutiveCorrect = 0;
         end
         
+        if iswrong == true
+            consecutiveWrong= consecutiveWrong + 1;
+        else
+            consecutiveWrong = 0;
+        end
+        
         order(i,1) = Y;
         tnum(index,:) = [];
         tcor(index,:) = [];
+        twrong(index,:)= [];
     end
     
     
     AllResults=trials(order,:);
     AllResults(:,9:10) = NaN;
-    
+    disp(AllResults)
     % now just check that everything is ok
     
     x = AllResults(:,7)';
